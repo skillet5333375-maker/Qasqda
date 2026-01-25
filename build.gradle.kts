9d6231fc5519b70524ec8881c2164525e2baa85a
skillet5333375-maker <skillet5333375@gmail.com>
2026-01-25 14:31:48 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,8 @@ plugins {
     id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
+// File: app/build.gradle.kts
+configurations.configureEach {
+    exclude(group = "com.android.support")
+}
 }
