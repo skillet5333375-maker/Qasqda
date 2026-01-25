42a9f30631a45e66b01244a68d60ad70a57aa8b8
skillet5333375-maker <skillet5333375@gmail.com>
2026-01-25 13:50:25 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,19 @@
+// File: settings.gradle.kts
 pluginManagement {
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Qasqda"
+include(":app")
